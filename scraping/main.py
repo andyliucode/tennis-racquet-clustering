ff8454168bb660766aca8dc0a979b7c4535cc691
Andy Liu <andy@liuonline.com>
2018-08-26 22:36:39 -0400

    Add scraping of racquet specs

--- scraping/main.py
+++ scraping/main.py
@@ -8,7 +8,17 @@ def main():
     catalog_directory = '/Headracquets.html'
     catalog_page = crawler.getHtml(index_url, catalog_directory)
     urls = crawler.getRacketUrls(catalog_page)
-    return urls
+
+    racquet_specs = []
+    for url in urls:
+        racquet_page = crawler.getHtml(url)
+        try:
+            racquet_spec = scraper.getRacquetSpecsDict(racquet_page)
+            racquet_specs.append(racquet_spec)
+        except:
+            print("Couldn't scrape ", url)
+
+    return racquet_specs
 
 
 if __name__ == '__main__':
--- scraping/scraper.py
+++ scraping/scraper.py
@@ -1,3 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+
+
+def getRacquetSpecsDict(racquet_page):
+    table = racquet_page.find('div', attrs={'class': 'rac_specs'}).table
+    # tbody tag is sometimes added
+    if table.find('tbody'):
+        table = table.tbody
+        
+    racquet_specs = {}
+    for row in table.findAll('tr', recursive=False):
+        key = row.td.find('strong')
+        key_text = key.text.strip(' :')
+
+        value = key.next_sibling
+        # skip <br> tags to find the next sibling
+        while(value.name == 'br'):
+            value = value.next_sibling
+
+        if(value.name == 'table'):
+            value_text = value.find('td').text
+        else:
+            value_text = value.strip(' /')
+
+        racquet_specs[key_text] = value_text
+    return racquet_specs
+
+
+def getReviewDate(review_page):
+    footer = review_page.find('div', attrs={'class': 'review_footer'}).p
+    review_date_sentence = footer.text.split('.')[0]
+    review_date_str = review_date_sentence.split(':')[1].strip()
+    
+    review_date = time.strptime(review_date_str, '%B %Y')
+    return review_date
+
